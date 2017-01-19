e279706beb9b397d909c289b542ad946c53c5395
skariel <skariel@gmail.com>
2017-01-19 10:07:48 +0200

    stuff

--- lib/all.jl
+++ lib/all.jl
@@ -1,3 +1,5 @@
+set_zero_subnormals(true)
+
 include("particles.jl")
 include("tree.jl")
 include("gravity.jl")
--- lib/testing.jl
+++ lib/testing.jl
@@ -1,4 +1,5 @@
 using Base.Test
+using Base.Threads
 
 function get_acc(particles, ix, eps2)
     ax=0.0
@@ -21,12 +22,28 @@ function perf(particles, ax, N, eps2)
     ixs = randperm(length(t.particles))[1:N]
     rax = zeros(N)
     vax = ax[ixs]
-    @inbounds for i in 1:N
+    @threads for i in 1:N
         tax,tay,taz = get_acc(particles, ixs[i], eps2)
         rax[i] = tax;
     end
     ee = abs((vax-rax)./rax*100)
-    sort(ee)[round(Int64, N*0.5)]
+    hx = linspace(-2,2,25)
+    mx = 0.5*(hx[2:end]+hx[1:(end-1)])
+    my = zeros(length(hx)-1)
+    for ei in ee
+        for i in 2:length(hx)
+            if log10(ei)<hx[i] && log10(ei)>hx[1]
+                my[i-1] += 1.0
+                break
+            end
+        end
+    end
+
+    ee50 = sort(ee)[round(Int64, N*0.5)]
+    ee90 = sort(ee)[round(Int64, N*0.9)]
+    ee95 = sort(ee)[round(Int64, N*0.9)]
+    ee99 = sort(ee)[round(Int64, N*0.99)]
+    mx,my, ee50,ee90,ee95,ee99, mean(log10(ee)), std(log10(ee))
 end
 
 function test_in_cell_mass(t::Tree)
@@ -456,3 +473,4 @@ function test_exp()
 
     @show pxy
 end
+
